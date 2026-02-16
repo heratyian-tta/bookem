4c7e42dab956f58072ce9de5fe3690ab791bb01d
ricealonzo <kingzoethebrand@gmail.com>
2026-02-16 23:24:03 +0000

    updated booking show to display guest name and location name. Added new booking link to location show page. changed root route to location#index. added create location  to nav bar.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :locations
   resources :bookings
- root to: "bookings#index"
+ root to: "locations#index"
   devise_for :users
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
   # get("/your_first_screen", { :controller => "pages", :action => "first" })
