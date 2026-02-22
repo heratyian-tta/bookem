faf628caaed59b496d392d446c09ba538329b83b
ricealonzo <kingzoethebrand@gmail.com>
2026-02-22 03:51:27 +0000

    fixed edit with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    extra_keys = [:first_name, :last_name, :organization, :phone_number, :website]
+    devise_parameter_sanitizer.permit(:sign_up, keys: extra_keys)
+    devise_parameter_sanitizer.permit(:account_update, keys: extra_keys)
+  end
 end
