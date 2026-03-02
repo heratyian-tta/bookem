b48a4f822f435bf367236e69d3793e6428e15d30
Alonzo Rice <96135977+ricealonzo@users.noreply.github.com>
2026-03-02 21:13:33 +0000

    ee

--- config/initializers/letter_opener.rb
+++ config/initializers/letter_opener.rb
@@ -1,4 +1,4 @@
-if Rails.developement?
+if Rails.env.developement?
   LetterOpener.configure do |config|
     # To overrider the location for message storage.
     # Default value is `tmp/letter_opener`
