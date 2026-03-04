fa5e7b32381ce275b85b569118675a4f3c3d2ad6
Alonzo Rice <96135977+ricealonzo@users.noreply.github.com>
2026-03-04 17:22:52 +0000

    updated web_rake.rb

--- config/initializers/web_rake.rb
+++ config/initializers/web_rake.rb
@@ -1,8 +1,6 @@
 # config/initializers/web_rake.rb
 
-if Rails.env.development? || Rails.env.tes?
-  WebRake.configure do |config|
-    config.username = 'Loveis1988'
-    config.password = 'Loveis1988'
-  end
+WebRake.configure do |config|
+  config.username = 'Loveis1988'
+  config.password = 'Loveis1988'
 end
